7470a2ef4643e35928ae5cfa9cdf5b4eeee37458
manickaselvi <57222412+manickaselvi@users.noreply.github.com>
2019-11-21 17:21:05 +0530

    RBStatementProcessConstants.java have been created
    Initial commit of RaboBank Customer statement processor

---
+++ main/java/com/rabobank/constants/RBStatementProcessConstants.java
@@ -0,0 +1,27 @@
+package com.rabobank.constants;
+
+/**
+ * @author manickaselvi m
+ *
+ */
+public class RBStatementProcessConstants {
+	
+	public static final Integer HTTP_CODE_SUCCESS = 200;
+	public static final Integer HTTP_CODE_ERROR = 500;
+	public static final Integer HTTP_CODE_INVALID_INPUT = 406;
+	
+	public static final String UNEXPECTED_SERVER_ERROR = "Unexpected server error!!";
+	public static final String VALIDATION_ERROR = "Validation error!!";
+	public static final String VALIDATION_SUCCESS = "Validation success!!";
+	public static final String INVALID_INPUT = "Invalid Input!!";
+	public static final String UNSUPPORTED_FILE_FORMAT = "Unsupported file format.Only csv or xml file allowed!!";
+	
+	public static final String FILE_TYPE_XML = "application/xml";
+	public static final String FILE_TYPE_CSV = "text/csv";
+	
+	public static final String ERROR_RECORDMAP_KEY_REFERENCE = "Validation failed by Reference";
+	public static final String ERROR_RECORDMAP_KEY_END_BAL = "Validation failed by End Balance";
+	
+	
+
+}
